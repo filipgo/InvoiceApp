2fa53e08519ea1b72774923dd972799ff00e8909
filipgo <filipgow@gmail.com>
2020-05-12 17:21:35 +0200

    changes in controler

--- ExampleInvoiceApp/Controllers/InvoiceController.cs
+++ ExampleInvoiceApp/Controllers/InvoiceController.cs
@@ -2,15 +2,25 @@ using System;
 using System.Threading.Tasks;
 using ExampleInvoiceApp.Common.Models;
 using ExampleInvoiceApp.Common.Models.Requests;
+using ExampleInvoiceApp.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ExampleInvoiceApp.App.Controllers
 {
     public class InvoiceController : Controller
     {
+        public InvoiceController(IInvoiceService invoiceService)
+        {
+            _invoiceService = invoiceService;
+        }
+
+        private readonly IInvoiceService _invoiceService;
+
         [HttpPost("create")]
         public async Task<IActionResult> Create(CreateInvoiceRequest request)
         {
+            await _invoiceService.Create(request);
+            
             throw new NotImplementedException();
         }
         
