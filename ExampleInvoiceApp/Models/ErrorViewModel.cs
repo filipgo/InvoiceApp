42644a55bb9aa7510a3b8392dd3d1c61f3828103
filipgo <filipgow@gmail.com>
2020-05-19 06:02:47 +0200

    coe cleanup

--- ExampleInvoiceApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ExampleInvoiceApp.App.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
